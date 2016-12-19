f36e34e067df106e4df48ffc911a8b5cf9260e79
JordanBecker21 <jordan.becker21@gmail.com>
2016-12-19 12:41:46 +0100

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,17 @@
+
+setwd("C:/Users/jordan.becker/Desktop/Amazon Drive/2016_Research/GitHub/Exploring Data Course Project 1")
+
+data<- read.table("household_power_consumption.txt", sep=";", header = T,
+                  stringsAsFactors = FALSE, dec = ".")
+
+
+data<- subset(data, data$Date %in% c("1/2/2007","2/2/2007"))
+
+data$Global_active_power<- as.numeric(data$Global_active_power)
+
+datetime <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+png("plot2.png", width = 480, height = 480)
+
+plot(datetime,data$Global_active_power,type = "l", ylab = "Global Active Power(kilowatts)")
+dev.off()
